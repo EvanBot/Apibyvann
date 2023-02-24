fdb2885fbe6a69f765ac0dd626fa5fa110abdd89
EvanBot <74948244+EvanBot@users.noreply.github.com>
2023-02-24 15:34:18 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 
 global.creator = 'VannxD'// yourname
 global.MONGO_DB_URI = "mongodb+srv://bj2684yu:bj2684yu@vannxd.e2fsoml.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.ACTIVATION_TOKEN_SECRET = "Prz838" //isi apa aja bebas
 global.your_email = "vincentpremium06@gmail.com" //email
-global.email_password = "adaz msjk dgka nrgt" //application password email
+global.email_password = "adazmsjkdgkanrgt" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
